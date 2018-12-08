a4e5d2ada37c93cc12a1a522f21d1a439cfbff35
haorengao <872248907@qq.com>
2018-12-08 13:17:44 +0800

    第一次版本提交

---
+++ src/main/java/com/hrg/demo_spring_security_01/DemoSpringSecurity01Application.java
@@ -0,0 +1,12 @@
+package com.hrg.demo_spring_security_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringSecurity01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringSecurity01Application.class, args);
+	}
+}
---
+++ src/test/java/com/hrg/demo_spring_security_01/DemoSpringSecurity01ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hrg.demo_spring_security_01;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoSpringSecurity01ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
